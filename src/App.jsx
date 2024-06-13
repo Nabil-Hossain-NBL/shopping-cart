b1f2713034dd6d3ac98d607efc5f825ee674fb7d
Nabil-Hossain-NBL <nabilhossainprf@gmail.com>
2024-06-13 12:18:23 +0600

    Use Reducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import "./App.css";
 import Header from "./components/Header";
+import { CartState } from "./context/Context";
 
 function App() {
+  const { state } = CartState();
+  console.log(state);
+
   return (
     <>
       <Header />
--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -1,11 +1,21 @@
-import { createContext } from "react";
+import { createContext, useContext, useReducer } from "react";
 
 import products from "../Data/data.json";
+import { cartReducer } from "./Reducers";
 
 const Cart = createContext();
 
 function Context({ children }) {
-  return <Cart.Provider value={products}>{children}</Cart.Provider>;
+  const [state, dispatch] = useReducer(cartReducer, {
+    products: products,
+    cart: [],
+  });
+
+  return <Cart.Provider value={{ state, dispatch }}>{children}</Cart.Provider>;
 }
 
 export default Context;
+
+export const CartState = () => {
+  return useContext(Cart);
+};
